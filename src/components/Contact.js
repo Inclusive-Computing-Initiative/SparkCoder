b8b090f39b30a54c3e8363df9e12d86586425c86
Zafir <zafir.nasim@gmail.com>
2024-02-02 20:21:24 -0600

    Replace percentage with fixed pixel amt

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,15 +1,15 @@
 import "../styles.css";
 import { Button } from "@mui/material/";
 
-export default function Courses() {
+export default function Contact() {
     return (
         <>
         <div className="page" style={{paddingTop:"15%",}}>
             <h1 className="title" style={{textAlign:"center", color:"#8BAFC8"}}> CONTACT </h1>
             <h1 className="title" style={{textAlign:"center", transform: "translate(0,-108%)", backgroundColor: "transparent"}}> CONTACT </h1>
-            <div style={{marginTop: "-12%", textAlign: "center"}}>
+            <div style={{marginTop: "-120px", textAlign: "center"}}>
                 <p style={{fontSize: "36px",letterSpacing: "3px", fontWeight: "700"}}>Have questions, concerns, or comments?</p>
-                <p style={{fontSize: "29px", letterSpacing: "2px", fontWeight: "300", marginTop: "-2%"}}>We'd love to hear your feedback!</p>
+                <p style={{fontSize: "29px", letterSpacing: "2px", fontWeight: "300", marginTop: "-24px"}}>We'd love to hear your feedback!</p>
                 <Button
                     variant="contained"
                     sx={{
@@ -23,7 +23,7 @@ export default function Courses() {
                     >
                     Feedback Form
                 </Button>
-                <p style={{fontSize: "24px", marginTop: "10%"}}> Visit our website: <a href="https://www.inclusivecompute.org/">https://www.inclusivecompute.org/</a></p>
+                <p style={{fontSize: "24px", marginTop: "100px"}}> Visit our website: <a href="https://www.inclusivecompute.org/">https://www.inclusivecompute.org/</a></p>
             </div>
         </div>
         </>
